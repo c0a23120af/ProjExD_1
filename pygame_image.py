3bc871ce5df4616ebeaa85e70f32f4d94e4aa09f
C0A23120 <C0A23120af@edu.teu.ac.jp>
2024-06-11 16:21:10 +0900

    練習問題６

--- pygame_image.py
+++ pygame_image.py
@@ -17,10 +17,15 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x=tmr%800
+        screen.blit(bg_img, [-x, 0])
+        
+        kk_rct = kk_img.get_rect()
+        kk_rct.center = 300, 200
+        screen.blit(kk_img,kk_rct)
         pg.display.update()
-        tmr += 1        
-        clock.tick(10)
+        tmr += 1   
+        clock.tick(200)
 
 
 if __name__ == "__main__":
