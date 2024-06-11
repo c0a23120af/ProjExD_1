08a41828301d3e461cf62b0d0e62b04f4a3f18d1
C0A23120 <C0A23120af@edu.teu.ac.jp>
2024-06-11 17:06:39 +0900

    練習問題8

--- pygame_image.py
+++ pygame_image.py
@@ -13,23 +13,32 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True,False)
+    kk_rect = kk_img.get_rect()
+    kk_rect.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         x=tmr%3200
-        screen.blit(bg_img2, [-x, 0]) 
-        screen.blit(bg_img, [-x+1600, 0]) 
-        screen.blit(bg_img2, [-x+3200, 0]) 
-        screen.blit(bg_img, [-x+4800, 0]) 
-        kk_rct = kk_img.get_rect()
-        kk_rct.center = 300, 200
-        screen.blit(kk_img,kk_rct)
+        screen.blit(bg_img, [-x, 0]) 
+        screen.blit(bg_img2, [-x+1600, 0]) 
+        screen.blit(bg_img, [-x+3200, 0]) 
+        screen.blit(bg_img2, [-x+4800, 0]) 
+        
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rect.move_ip(0, -1)
+        elif  key_lst[pg.K_DOWN]:
+            kk_rect.move_ip(0, +1)
+        elif  key_lst[pg.K_LEFT]:
+            kk_rect.move_ip(-1, 0)
+        elif  key_lst[pg.K_RIGHT]:
+            kk_rect.move_ip(+1, 0)
+        screen.blit(kk_img, kk_rect)
         pg.display.update()
         tmr += 1   
         clock.tick(200)
-
 
 if __name__ == "__main__":
     pg.init()
